5cf1cd346e07b89ffa8614c5427b5526d8d6b671
AmirulAzam007 <amirulazam115@gmail.com>
2026-03-07 17:34:07 +0600

    Login-page

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("lgn-btn").addEventListener("click", function(){
+
+    const usernameIn = document.getElementById("usernameIn");
+    const username = usernameIn.value;
+
+    const pass = document.getElementById("pass");
+    const password =  pass.value;
+
+    if(username=="admin" && password=="admin123")
+    {
+
+        window.location.assign("/home.html");
+    }
+    else
+    {
+        alert("Wrong Info");
+    }
+
+    
+})
